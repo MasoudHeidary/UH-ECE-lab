b21448a89e2a3bcbda67b00a6bcc433f9090f7f7
Masoud Heidary <Masoud.hei.dev@gmail.com>
2024-05-16 15:07:38 -0500

    +plot

--- MachineModel/plot.py
+++ MachineModel/plot.py
@@ -10,17 +10,17 @@ resnet18Mani_delta1 = [0.8666, 0.8521, 0.8258, 0.7981]
 vgg11Mani_delta1 = [0.8614, 0.8388, 0.8121, 0.7828]
 vgg19Mani_delta1 = [0.8891, 0.8766, 0.8578, 0.833]
 
-plt.plot(manipulate_percentage, darknetMani_delta1, linewidth=2, label="darknet d[1]")
-plt.plot(manipulate_percentage, squeezenetMani_delta1, linewidth=2, label="squeeze d[1]")
+# plt.plot(manipulate_percentage, darknetMani_delta1, linewidth=2, label="darknet d[1]")
+# plt.plot(manipulate_percentage, squeezenetMani_delta1, linewidth=2, label="squeeze d[1]")
 plt.plot(manipulate_percentage, resnet18Mani_delta1, linewidth=2, label="resnet d[1]")
 plt.plot(manipulate_percentage, vgg11Mani_delta1, linewidth=2, label="vgg11 d[1]")
-plt.plot(manipulate_percentage, vgg19Mani_delta1, linewidth=2, label="vgg19 d[1]")
+# plt.plot(manipulate_percentage, vgg19Mani_delta1, linewidth=2, label="vgg19 d[1]")
 
-plt.text(manipulate_percentage[-1], darknetMani_delta1[-1], "darknet d[1]", fontsize=14, ha='right', va='bottom') 
-plt.text(manipulate_percentage[-1], squeezenetMani_delta1[-1], "squeeze d[1]", fontsize=14, ha='right', va='bottom') 
-plt.text(manipulate_percentage[-1], resnet18Mani_delta1[-1], "resnet d[1]", fontsize=14, ha='right', va='bottom') 
-plt.text(manipulate_percentage[-1], vgg11Mani_delta1[-1], "vgg11 d[1]", fontsize=14, ha='right', va='bottom') 
-plt.text(manipulate_percentage[-1], vgg19Mani_delta1[-1], "vgg19 d[1]", fontsize=14, ha='right', va='bottom') 
+plt.text(manipulate_percentage[-1], darknetMani_delta1[-1], "darknet d[1]", fontsize=14, fontweight='bold', ha='right', va='bottom') 
+plt.text(manipulate_percentage[-1], squeezenetMani_delta1[-1], "squeeze d[1]", fontsize=14, fontweight='bold', ha='right', va='bottom') 
+plt.text(manipulate_percentage[-1], resnet18Mani_delta1[-1], "resnet d[1]", fontsize=14, fontweight='bold', ha='right', va='bottom') 
+plt.text(manipulate_percentage[-1], vgg11Mani_delta1[-1], "vgg11 d[1]", fontsize=14, fontweight='bold', ha='right', va='bottom') 
+plt.text(manipulate_percentage[-1], vgg19Mani_delta1[-1], "vgg19 d[1]", fontsize=14, fontweight='bold', ha='right', va='bottom') 
 
 # network Cipher10 [epoch:100], using x2 delta
 darknetMani_delta2 = [0.8358, 0.5617, 0.2006, 0.1135,]
@@ -29,22 +29,24 @@ resnet18Mani_delta2 = [0.8666, 0.7486, 0.5593, 0.3686,]
 vgg11Mani_delta2 = [0.8614, 0.7226, 0.4991, 0.3071,]
 vgg19Mani_delta2 = [0.8891, 0.776, 0.4828, 0.1949,]
 
-plt.plot(manipulate_percentage, darknetMani_delta2, linewidth=5, label="darknet d[2]")
-plt.plot(manipulate_percentage, squeezenetMani_delta2, linewidth=5, label="squeeze d[2]")
+# plt.plot(manipulate_percentage, darknetMani_delta2, linewidth=5, label="darknet d[2]")
+# plt.plot(manipulate_percentage, squeezenetMani_delta2, linewidth=5, label="squeeze d[2]")
 plt.plot(manipulate_percentage, resnet18Mani_delta2, linewidth=5, label="resnet d[2]")
 plt.plot(manipulate_percentage, vgg11Mani_delta2, linewidth=5, label="vgg11 d[2]")
-plt.plot(manipulate_percentage, vgg19Mani_delta2, linewidth=5, label="vgg19 d[2]")
+# plt.plot(manipulate_percentage, vgg19Mani_delta2, linewidth=5, label="vgg19 d[2]")
 
-plt.text(manipulate_percentage[-1], darknetMani_delta2[-1], "darknet d[2]", fontsize=14, ha='right', va='bottom') 
-plt.text(manipulate_percentage[-1], squeezenetMani_delta2[-1], "squeeze d[2]", fontsize=14, ha='right', va='bottom') 
-plt.text(manipulate_percentage[-1], resnet18Mani_delta2[-1], "resnet d[2]", fontsize=14, ha='right', va='bottom') 
-plt.text(manipulate_percentage[-1], vgg11Mani_delta2[-1], "vgg11 d[2]", fontsize=14, ha='right', va='bottom') 
-plt.text(manipulate_percentage[-1], vgg19Mani_delta2[-1], "vgg19 d[2]", fontsize=14, ha='right', va='bottom') 
+plt.text(manipulate_percentage[-1], darknetMani_delta2[-1], "darknet d[2]", fontsize=14, fontweight='bold', ha='right', va='bottom') 
+plt.text(manipulate_percentage[-1], squeezenetMani_delta2[-1], "squeeze d[2]", fontsize=14, fontweight='bold', ha='right', va='bottom') 
+plt.text(manipulate_percentage[-1], resnet18Mani_delta2[-1], "resnet d[2]", fontsize=14, fontweight='bold', ha='right', va='bottom') 
+plt.text(manipulate_percentage[-1], vgg11Mani_delta2[-1], "vgg11 d[2]", fontsize=14, fontweight='bold', ha='right', va='bottom') 
+plt.text(manipulate_percentage[-1], vgg19Mani_delta2[-1], "vgg19 d[2]", fontsize=14, fontweight='bold', ha='right', va='bottom') 
 
 
 
 plt.xlabel('manipulate percentage', fontsize=16, fontweight='bold')
+plt.xticks(fontsize=14, fontweight='bold')
 plt.ylabel('network accuract', fontsize=16, fontweight='bold')
+plt.yticks(fontsize=14, fontweight='bold')
 # plt.title('Title', fontsize=16, fontweight='bold')
 plt.grid(True)
 # plt.legend(fontsize=12)
