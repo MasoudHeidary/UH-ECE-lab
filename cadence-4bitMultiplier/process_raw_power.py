ea36ae64c364f315e48f6e057628bb2c01fae1b1
Masoud Heidary <Masoud.hei.dev@gmail.com>
2024-05-24 13:15:29 -0500

    + plot energy

--- cadence-4bitMultiplier/process_raw_power.py
+++ cadence-4bitMultiplier/process_raw_power.py
@@ -48,13 +48,22 @@ plt.show()
 normal_energy = [i*j for (i,j) in zip(normal_power, normal_delay)]
 modified_energy = [i*j for (i,j) in zip (modified_power, modified_delay)]
 
-plt.title('Energy', fontsize=16)
-plt.plot(x, normal_energy, label="normal", linewidth=2.5)
-plt.plot(x, modified_energy, label="modified", linewidth=2.5)
-plt.xlabel('time(weeks)', fontsize=16)
-plt.ylabel('energy(uW*ps)', fontsize=16)
-plt.tick_params(axis='both', labelsize=16)
-plt.legend()
+normal_energy = [i/1000 for i in normal_energy]
+modified_energy = [i/1000 for i in modified_energy]
+
+# plt.title('Energy', fontsize=16)
+plt.plot(x, normal_energy, label="normal", linewidth=3)
+plt.plot(x, modified_energy, label="modified", linewidth=3)
+
+plt.xlabel('time(weeks)', fontsize=14, fontweight='bold')
+plt.xticks(fontsize=14, fontweight='bold')
+
+plt.ylabel('energy(fJ)', fontsize=14, fontweight='bold')
+plt.yticks(fontsize=14, fontweight='bold')
+
+# plt.tick_params(axis='both', labelsize=14)
+
+plt.legend(fontsize=14)
 plt.grid(True)
 plt.show()
 
