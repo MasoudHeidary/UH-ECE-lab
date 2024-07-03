e75e6ac67e4e110302c2b3a01dd32b8a1747ab5b
Masoud Heidary <Masoud.hei.dev@gmail.com>
2024-07-03 12:42:00 -0500

    Multibody multiplier ocean

--- arhieve-cadence/ocean_Multiplier_multiBody.py
+++ arhieve-cadence/ocean_Multiplier_multiBody.py
@@ -441,27 +441,25 @@ counter = 0
 counter_t = 0
 
 for vdd in [i/100 for i in range(60, 90+5, 10)]:
-    vb_base = int(vdd*100)
-    vb_max = 150+5
-    for pb0 in [i/100 for i in range(vb_base, vb_max, 30)]:
-        for pb1 in [i/100 for i in range(vb_base, vb_max, 30)]:
-            for pb2 in [i/100 for i in range(vb_base, vb_max, 30)]:
-                for pb3 in [i/100 for i in range(vb_base, vb_max, 30)]:
-                    for pb4 in [i/100 for i in range(vb_base, vb_max, 30)]:
-                        for pb5 in [i/100 for i in range(vb_base, vb_max, 30)]:
-                            for pb6 in [i/100 for i in range(vb_base, vb_max, 30)]:
+    # vb_base = int(vdd*100)
+    vb_base = 200
+    vb_max = 350+1
+    for pb0 in [i/100 for i in range(vb_base, vb_max, 50)]:
+        for pb1 in [i/100 for i in range(vb_base, vb_max, 50)]:
+            for pb2 in [i/100 for i in range(vb_base, vb_max, 50)]:
+                for pb3 in [i/100 for i in range(vb_base, vb_max, 50)]:
+                    for pb4 in [i/100 for i in range(vb_base, vb_max, 50)]:
+                        for pb5 in [i/100 for i in range(vb_base, vb_max, 50)]:
+                            for pb6 in [i/100 for i in range(vb_base, vb_max, 50)]:
 
-                                if 22607 < counter:
-                                    # pb = [pb0, pb1, pb2, pb3, pb4, pb5, pb6]
-                                    # netlist = generate_netlist(pb, vdd)
-                                    # update_netlist_file("/home/mheidary/simulation/test_MPnb2/spectre/schematic/netlist/netlist", netlist)
+                                if 0 < counter:
+                                    pb = [pb0, pb1, pb2, pb3, pb4, pb5, pb6]
+                                    netlist = generate_netlist(pb, vdd)
+                                    update_netlist_file("/home/mheidary/simulation/test_MPnb2/spectre/schematic/netlist/netlist", netlist)
 
-                                    # script = generate_ocean_script(f"./log/{counter}", pb, vdd)
-                                    # update_ocean_script_file("./MP.ocn", script)
-                                    # run_ocean_script("./MP.ocn")
-                                    pass
-                                    counter_t += 1
-                                    print(f"t: {counter_t}")
+                                    script = generate_ocean_script(f"./log/{counter}", pb, vdd)
+                                    update_ocean_script_file("./multibody.ocn", script)
+                                    run_ocean_script("./multibody.ocn")
 
                                 counter += 1
                                 print(counter)
