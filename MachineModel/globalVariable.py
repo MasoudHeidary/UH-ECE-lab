df94c8c6d94c694da06d69622a324f514df6ee67
Masoud Heidary <Masoud.hei.dev@gmail.com>
2024-07-16 11:18:56 -0500

    cifar10 manipulate 4%

--- MachineModel/globalVariable.py
+++ MachineModel/globalVariable.py
@@ -4,26 +4,27 @@ default_epoch_range = 200
 default_lr = 0.5
 default_weight_decay = 0.0005
 
-spc_epoch_range = 200
-spc_lr = [0.1]*1 + [0.05]*1 + [0.02]*60 + [0.004]*40 + [0.0008]*40
-spc_weight_decay = 5e-4
-spc_momentum = 0.9
+# spc_epoch_range = 200
+# spc_lr = [0.1]*1 + [0.05]*1 + [0.02]*60 + [0.004]*40 + [0.0008]*40
+# spc_weight_decay = 5e-4
+# spc_momentum = 0.9
 
-'Cifar10'
-'Cifar100'
-'SVHN'
-DATA_SET = 'Cifar100'
+# 'Cifar10'
+# 'Cifar100'
+# 'SVHN'
+DATA_SET = 'Cifar10'
 
-TRAIN_FLAG = True
+TRAIN_FLAG = False
 CONFIRM_TO_TRAIN = True
 
 LOG_FILE_NAME = False 
 
+# device to do the computation on
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 if torch.cuda.is_available():
     torch.cuda.set_device(0)
 
-default_manipulate_range = range(0, 5+1, 1)
+default_manipulate_range = range(0, 4+1, 1)
 default_manipulate_divider = 1
 delta = torch.tensor(
     [1, 0.5, 0.25, 0.125, 0.0675,
--- MachineModel/plot.py
+++ MachineModel/plot.py
@@ -24,13 +24,13 @@ vgg19Mani_delta1 = [0.8891, 0.8766, 0.8578, 0.833]
 # plt.text(manipulate_percentage[-1], vgg19Mani_delta1[-1], "vgg19 d[1]", fontsize=14, fontweight='bold', ha='right', va='bottom') 
 
 # network Cipher10 [epoch:100], using x2 delta
-darknetMani_delta2 = [0.8358, 0.5617, 0.2006, 0.1135,]
-squeezenetMani_delta2 = [0.8397, 0.2692, 0.1251, 0.1053,]
-resnet18Mani_delta2 = [0.8666, 0.7486, 0.5593, 0.3686,]
-vgg11Mani_delta2 = [0.8614, 0.7226, 0.4991, 0.3071,]
-vgg19Mani_delta2 = [0.8891, 0.776, 0.4828, 0.1949,]
+darknetMani_delta2 = [0.8358, 0.5617, 0.2006, 0.1135, 0.1034]
+squeezenetMani_delta2 = [0.8397, 0.2692, 0.1251, 0.1053, 0.1010]
+resnet18Mani_delta2 = [0.8666, 0.7486, 0.5593, 0.3686, 0.2440]
+vgg11Mani_delta2 = [0.8614, 0.7226, 0.4991, 0.3071, 0.2055]
+vgg19Mani_delta2 = [0.8891, 0.776, 0.4828, 0.1949, 0.1184]
 
-if False:
+if True:
     plt.plot(manipulate_percentage, darknetMani_delta2, linewidth=3, label="darknet")
     plt.plot(manipulate_percentage, squeezenetMani_delta2, linewidth=3, label="squeeze")
     plt.plot(manipulate_percentage, resnet18Mani_delta2, linewidth=3, label="resnet")
