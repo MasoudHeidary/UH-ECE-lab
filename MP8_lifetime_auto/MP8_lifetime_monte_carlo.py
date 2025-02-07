46aa8deb6ff064af8717d84614a9d76da596c642
Masoud Heidary <Masoud.hei.dev@gmail.com>
2025-02-07 15:52:14 -0600

    2s compliment: monte carlo lifetime

--- MP8_lifetime_auto/MP8_lifetime_monte_carlo.py
+++ MP8_lifetime_auto/MP8_lifetime_monte_carlo.py
@@ -5,9 +5,14 @@ from tool.log import Log
 
 from msimulator.get_alpha_class import MultiplierStressTest
 from msimulator.Multiplier import MPn_v3
-from get_life_expect import get_life_expect
+from get_life_expect import get_life_expect, generate_random_vth_base, generate_body_voltage_from_base
 from sympy import symbols, Or, And, Not, simplify_logic
 from pyeda.inter import expr
+
+import tool.NBTI_formula as NBTI
+import tool.vth_body_map as VTH
+from tool.map_pb_to_current import get_current_from_pb, get_pb_from_current
+
 
 # cache
 import pickle
@@ -146,7 +151,7 @@ equation_conf = [
 ### ideal computations
 ##############################
 
-if True:
+if False:
     log = Log(f"{__file__}.log", terminal=True)
     DETAIL_LOG = False
 
@@ -186,7 +191,7 @@ if True:
                 log.println(f"faulty_transistor: \t{faulty_transistor} => failed_transistor: \t {failed_transistor}")
 
     # final result
-    log.println(f"conf:\n{equation_conf}\n")
+    log.println(f"conf:\n{equation_conf}")
     log.println(f"final result >>> sum lifetime {sum_lifetime} / transistor len {len_transistor} = {sum_lifetime/len_transistor}")
 
 
@@ -198,3 +203,87 @@ if True:
 ##############################
 ### monte carlo sampling computations
 ##############################
+
+def get_monte_carlo_life_expect(alpha, vth_matrix, bit_len=bit_len):
+    initial_v_base = VTH.get_body_voltage(vth=abs(NBTI.Vth))
+    current_base = get_current_from_pb(initial_v_base)
+    current_fail = current_base * (1 - 0.5)
+    vb_fail = get_pb_from_current(current_fail)
+
+    for t_week in range(200):
+        t_sec = t_week * 7 * 24 * 60 * 60
+        body_voltage = generate_body_voltage_from_base(
+            alpha, t_sec, bit_len, vth_matrix
+        )
+
+        for fa_i in range(bit_len - 1):
+            for fa_j in range(bit_len):
+                for t_index in range(6):
+                    if body_voltage[fa_i][fa_j][t_index] >= vb_fail:
+                        return {
+                            "fa_i": fa_i,
+                            "fa_j": fa_j,
+                            "t_index": t_index,
+                            "t_week": t_week,
+                        }
+
+
+if True:
+    log = Log(f"{__file__}.log", terminal=True)
+    SAMPLE = 10
+    DETAIL_LOG = True
+
+    # loading alpha cache
+    try:
+        alpha_cache = CACHE.load_cache(f"{__file__}.alpha.cache")
+    except:
+        alpha_cache = CACHE(filename=f"{__file__}.alpha.cache")
+
+    sum_lifetime = 0
+
+    # base alpha
+    base_alpha = None
+    if not alpha_cache.hit_cache('0'):
+        optimizer_equation[0] = "0"
+        base_alpha = MultiplierStressTest(bit_len, eq_optimizer_trigger, eq_optimizer_accept).run(log_obj=False)
+        log.println("Normal alpha calculated")
+    else:
+        base_alpha = alpha_cache.get_cache("0")
+        log.println(f"Normal alpha loaded")
+
+    # preload equations alpha
+    for conf in equation_conf:
+        if not alpha_cache.hit_cache(conf["equation"]):
+            optimizer_equation[0] = conf["equation"]
+            conf['alpha'] = MultiplierStressTest(bit_len, eq_optimizer_trigger, eq_optimizer_accept).run(log_obj=False)
+        else:
+            conf['alpha'] = alpha_cache.get_cache(conf["equation"])
+    log.println("equation alpha loaded")
+
+    for sample_index in range(SAMPLE):
+
+        random_vth_matrix = generate_random_vth_base()
+        base_fail_transistor = get_monte_carlo_life_expect(base_alpha, random_vth_matrix, bit_len)
+        base_lifetime = base_fail_transistor["t_week"]
+
+        # choose optimizer
+        optimize_equation = None
+        optimize_alpha = None
+        for conf in equation_conf:
+            if (base_fail_transistor['fa_i'], base_fail_transistor['fa_j'], base_fail_transistor['t_index']) in conf['transistor_list']:
+                optimize_equation = conf['equation']
+                optimize_alpha = conf['alpha']
+                break
+
+        optimized_fail_transistor = get_monte_carlo_life_expect(optimize_alpha, random_vth_matrix, bit_len)
+        optimized_lifetime = optimized_fail_transistor['t_week']
+
+
+        if DETAIL_LOG:
+            log.println(f"[{base_lifetime:3d}] -> [{optimized_lifetime:3d}] \t|| {base_fail_transistor} => {optimize_equation} => {optimized_fail_transistor}")
+
+        
+    # final result
+    log.println(f"conf:\n{equation_conf}\n")
+    log.println(f"final result >>> sum lifetime {sum_lifetime} / samples {SAMPLE} = {sum_lifetime/SAMPLE}")
+
